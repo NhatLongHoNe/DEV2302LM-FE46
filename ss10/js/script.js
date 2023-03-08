9f735764bb085515e20b67a351fe76789d534c1a
NhatLongHo <62054995+NhatLongHoNe@users.noreply.github.com>
2023-03-08 20:08:05 +0700

    Add files via upload
    ss10

---
+++ ss10/js/script.js
@@ -0,0 +1,13 @@
+
+// làm lab 7 - thực hành 2
+
+$(document).ready(function(){
+   // $("#txtUserName").hide();
+
+    $("#btn-click").click(function(){
+        var userName = $("#txtUserName").val();
+
+        console.log(userName)
+    })
+
+});
